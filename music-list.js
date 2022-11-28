c8b38acdd7f57424f8f95198d9c1887df44b576a
whithnta <tuananh242003@gmail.com>
2022-11-28 23:59:46 +0700

    first commit

---
+++ music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "XTC",
+      artist: "RPT Groovie ft TLinh x RPT MCK",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Tại Vì Sao",
+      artist: "MCKAY",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Chìm Sâu",
+      artist: "MCKAY",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Flexin' trên Circle K",
+      artist: "LOW-G",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "An Thần ",
+      artist: "Thắng & LOW G",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Vì Anh Đâu Có Biết",
+      artist: "Madihu & Vũ",
+      img: "music-6",
+      src: "music-6"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
